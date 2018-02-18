92cc7e05ecaa02098d25cc554943d53c9299cb4f
fatihinan <mfatihinan@gmail.com>
2018-02-18 03:31:17 -0500

    Constantly shooting feature is added

--- Space Invaders/GameScene/GameScene.swift
+++ Space Invaders/GameScene/GameScene.swift
@@ -9,34 +9,84 @@
 import SpriteKit
 import GameplayKit
 
+let screenSize = UIScreen.main.bounds
+var screenWidth: CGFloat?
+
+var screenHeight: CGFloat?
+
 class GameScene: SKScene,SKPhysicsContactDelegate {
     
+    var boss: SKSpriteNode?
+    var weapon: SKSpriteNode?
+    var greenBug: SKSpriteNode?
+    var redBug: SKSpriteNode?
+    var blueBug: SKSpriteNode?
+    var bullet: SKSpriteNode?
+    
+    
+//    var redBug2: SKSpriteNode?
+//    var redBug3: SKSpriteNode?
+//    var redBug4: SKSpriteNode?
+//    var redBug5: SKSpriteNode?
+//    var redBug6: SKSpriteNode?
+//    var redBug7: SKSpriteNode?
+//    var redBug8: SKSpriteNode?
+    
+    
+    var fireRate:TimeInterval = 0.5
+    var timeSinceFire:TimeInterval = 0
+    var lastTime:TimeInterval = 0
     
     override func didMove(to view: SKView) {
-    
-        let boss: SKSpriteNode = self.childNode(withName: "boss") as! SKSpriteNode
-        let weapon: SKSpriteNode = self.childNode(withName: "weapon") as! SKSpriteNode
+        
+        screenWidth = frame.width
+        screenHeight = frame.height
+        boss = self.childNode(withName: "boss") as! SKSpriteNode
+        weapon = self.childNode(withName: "weapon") as! SKSpriteNode
+        greenBug = self.childNode(withName: "greenBug") as! SKSpriteNode
+        redBug = self.childNode(withName: "redBug") as! SKSpriteNode
+        blueBug = self.childNode(withName: "blueBug") as! SKSpriteNode
 
- 
+
+//        redBug2 = self.childNode(withName: "redBug2") as? SKSpriteNode
+//        redBug3 = self.childNode(withName: "redBug3") as? SKSpriteNode
+//        redBug4 = self.childNode(withName: "redBug4") as? SKSpriteNode
+//        redBug5 = self.childNode(withName: "redBug5") as? SKSpriteNode
+//        redBug6 = self.childNode(withName: "redBug6") as? SKSpriteNode
+//        redBug7 = self.childNode(withName: "redBug7") as? SKSpriteNode
+//        redBug8 = self.childNode(withName: "redBug8") as? SKSpriteNode
+
+        self.physicsWorld.contactDelegate = self
+        
+        // play background music
+        let music = SKAudioNode(fileNamed: "spacemusic")
+        self.addChild(music)
+        music.autoplayLooped = true
+        
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        <#code#>
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-       
+        self.weapon?.position = CGPoint(x: pos.x, y: 50.0)
+
         
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        
+        self.weapon?.position = CGPoint(x: pos.x, y: 50.0)
     }
     
     func touchUp(atPoint pos : CGPoint) {
-       
+        self.weapon?.position = CGPoint(x: pos.x, y: 50.0)
+
         
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
         
         
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
@@ -56,6 +106,56 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        //redBug?.physicsBody?.velocity = CGVector(dx: 25, dy: 0)
+        
+        checkBullet(currentTime - lastTime)
+        lastTime = currentTime
+        
+//        if(ScoreManager.Lives > 0) {
+//            livesLabel?.text = "Lives: \(ScoreManager.Lives)"
+//            scoreLabel?.text = "Score: \(ScoreManager.Score)"
+//        }
+//        else {
+//            if let view = self.view {
+//                if let scene = SKScene(fileNamed: "GameOverScene") {
+//                    scene.scaleMode = .aspectFit
+//                    view.presentScene(scene)
+//                }
+//            }
+//        }
+        
+        }
+    
+    
+
+    
+    func checkBullet(_ frameRate: TimeInterval)
+    {
+        // add time to timer
+        timeSinceFire += frameRate
+        
+        // return if it hasn't been enough time to fire laser
+        if timeSinceFire < fireRate {
+            return
+        }
+        
+        shootBullet()
+        
+        // reset timer
+        timeSinceFire = 0
     }
+    
+    
+    func shootBullet() {
+        let scene:SKScene = SKScene(fileNamed: "Bullet")!
+        let bullet = scene.childNode(withName: "bullet")
+        bullet?.position = weapon!.position
+        bullet?.move(toParent: self)
+    }
+    
+    
 }
+
+
+
+
