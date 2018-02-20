0086b8e76707fc0fd65434bcba049061d328789d
Robert <robert.a.argume@gmail.com>
2018-02-19 20:34:43 -0500

    Fix bug that makes the game reset constantly
    Also solve bug that makes the defender hero to move in steps instead of fluently

--- Space Invaders/GameViewController.swift
+++ Space Invaders/GameViewController.swift
@@ -35,20 +35,6 @@ class GameViewController: UIViewController {
     override var shouldAutorotate: Bool {
         return true
     }
-    
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene, transition: SKTransition.crossFade(withDuration: 1.0))
-            }
-        }
-    }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
