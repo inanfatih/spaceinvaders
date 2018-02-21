a2377933b351ecf4e9247c578446e902144094bc
Robert <robert.a.argume@gmail.com>
2018-02-21 17:30:38 -0500

    Add Instructions Page. Add Game image to welcome page

---
+++ Space Invaders/GameScene/InstructionsScene.swift
@@ -0,0 +1,57 @@
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+
+class InstructionsScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    func touchDown(atPoint pos : CGPoint) {
+        
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for t in touches { self.touchDown(atPoint: t.location(in: self))}
+        ScoreManager.Lives = 5
+        ScoreManager.Score = 0
+        
+        if let view = self.view {
+            if let scene = SKScene(fileNamed: "GameScene") {
+                scene.scaleMode = .aspectFit
+                view.presentScene(scene)
+            }
+        }
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+    
+}
+
--- Space Invaders/GameScene/MainMenu.swift
+++ Space Invaders/GameScene/MainMenu.swift
@@ -7,7 +7,6 @@ import GameplayKit
 class MainMenu: SKScene {
     
     override func didMove(to view: SKView) {
-     
     }
     
     func touchDown(atPoint pos : CGPoint) {
@@ -24,17 +23,37 @@ class MainMenu: SKScene {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-        
-        ScoreManager.Lives = 5
-        ScoreManager.Score = 0
-        
-        if let view = self.view {
-            if let scene = SKScene(fileNamed: "GameScene") {
-                scene.scaleMode = .aspectFit
-                view.presentScene(scene)
+        for t in touches {
+            let positionInScene = t.location(in: self)
+            let touchedNode = self.atPoint(positionInScene)
+            
+            if let name = touchedNode.name
+            {
+                if name == "InstructionsButton"
+                {
+                    if let view = self.view {
+                        if let scene = SKScene(fileNamed: "InstructionsScene") {
+                            scene.scaleMode = .aspectFit
+                            view.presentScene(scene)
+                        }
+                    }
+                }
             }
+            else{
+                ScoreManager.Lives = 5
+                ScoreManager.Score = 0
+                
+                if let view = self.view {
+                    if let scene = SKScene(fileNamed: "GameScene") {
+                        scene.scaleMode = .aspectFit
+                        view.presentScene(scene)
+                    }
+                }
+            }
+            //self.touchDown(atPoint: t.location(in: self))
         }
+       
+        
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
